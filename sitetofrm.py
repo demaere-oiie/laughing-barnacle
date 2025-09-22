a34581e51e835b854c0f4f0d79c8c80e5723180f
demaere-oiie <47918368+demaere-oiie@users.noreply.github.com>
2025-09-22 15:07:11 +0200

    principal downsets and their representatives

--- sitetofrm.py
+++ sitetofrm.py
@@ -31,6 +31,23 @@ m3 = set("""
 01xyz
 """.split())
 
+def alpha(ds):
+    return set(c for d in ds for c in d)
+
+def le(x,y,ds):
+    return all(x in d for d in ds if y in d)
+
+# generate the principal downset of x
+def lower(x,ds):
+    return "".join(sorted(y for y in alpha(ds) if le(y,x,ds)))
+
+# if d is a principal downset, return its representative
+def rep(d,ds):
+    for c in d:
+        if all(le(y,c,ds) for y in d):
+            return c
+    return None
+
 # check that a system of downsets is closed under intersection and union
 def assert_closure(ds):
   for x in ds:
@@ -47,6 +64,14 @@ def assert_distr(ds):
          b = union(isect(x,y),isect(x,z))
          assert(a == b)
 
+# check that lower and rep are inverses
+def assert_reps(ds):
+   for s in alpha(ds):
+       assert(rep(lower(s,ds),ds)==s)
+   for d in ds:
+       if rep(d,ds): # ignore non-principal downsets
+           assert(lower(rep(d,ds),ds)==d)
+
 if __name__=="__main__":
     # we should've ensured this by hand
     assert_closure(n5)
@@ -55,3 +80,7 @@ if __name__=="__main__":
     # the lattices are not distributive, but their downsets are
     assert_distr(n5)
     assert_distr(m3)
+
+    # lower and rep should be inverse on principal downsets
+    assert_reps(n5)
+    assert_reps(m3)
