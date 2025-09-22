13397715fa6c3320d1a636e106239c54308c2d42
demaere-oiie <47918368+demaere-oiie@users.noreply.github.com>
2025-09-22 14:17:08 +0200

    Create sitetofrm.py

---
+++ sitetofrm.py
@@ -0,0 +1,26 @@
+# work through p.47, def'n 4.4.1 and thm 4.4.2 of Vickers, *Topology via Logic*, via concrete examples
+
+# concrete downsets
+# N5: pentagon lattice
+n5 = set("""
+0
+0x
+0xz
+0y
+0xy
+0xyz
+01xyz
+""".split())
+
+# M3: diamond lattice
+m3 = set("""
+0
+0x
+0y
+0z
+0xy
+0xz
+0yz
+0xyz
+01xyz
+""".split())
